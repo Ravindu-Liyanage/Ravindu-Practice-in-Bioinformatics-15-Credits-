06568e92e9f5fb9137c4718f943c3a4d3ab214c7
Ravindu Liyanage <148881716+Ravindu-Liyanage@users.noreply.github.com>
2026-09-07 11:46:54 +0200

    Delete 4. nnUnet AI model directory

--- 4. nnUnet AI model/env_nnunet.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["nnUNet_raw"] = r"R:\Practice-in-Bioinfo\nnUnet\nnUNet_raw"
-os.environ["nnUNet_preprocessed"] = r"R:\Practice-in-Bioinfo\nnUnet\nnUNet_preprocessed"
-os.environ["nnUNet_results"] = r"R:\Practice-in-Bioinfo\nnUnet\nnUNet_results"
